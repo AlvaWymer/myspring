9cf8cf0a4406a03ac25f366fd56ac73db2887c91
yangnaihua.2008@163.com <sdrzynhua.2013>
2017-05-10 20:00:22 +0800

    diyici

---
+++ myspring-resource/src/main/java/cn/mldn/myspring/resource/App.java
@@ -0,0 +1,9 @@
+package cn.mldn.myspring.resource;
+
+public class App {
+	public static void main(String[] args) {
+//		System.getProperties().list(System.out);
+//		获取当前工程路径
+		System.out.println(System.getProperty("user.dir"));
+	}
+}
